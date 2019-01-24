881bdc49d065e15579102429f1bfd7b8c248a2f9
PoojaReddyKalagiri <33696960+PoojaReddyKalagiri@users.noreply.github.com>
2019-01-24 13:43:30 +0530

    Add files via upload

---
+++ characterCount.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class characterCount{
+    public static void main(String[] args) throws Exception {
+        Map<Character, Integer> characterCount = new TreeMap<Character, Integer>();
+        /*for(char c='a';c<='z';c++){
+            characterCount.put(c,0);
+        }*/
+        File file = new File("/home/zadmin/file_char_count"); //input file
+        FileWriter fw = new FileWriter("/home/zadmin/outputfile.txt");
+
+        Scanner sc= new Scanner(file);
+        while(sc.hasNextLine()) {
+            char[] chars = sc.nextLine().toLowerCase().toCharArray();
+            for (Character ch : chars) {
+                if (characterCount.containsKey(ch)) {
+                    characterCount.put(ch, characterCount.get(ch) + 1);
+                }
+                else
+                    characterCount.put(ch,1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : characterCount.entrySet()) {
+            String s=entry.getKey()+" : "+ entry.getValue() + "\n";
+            fw.write(s);
+
+        }
+        fw.close();
+    }
+}
